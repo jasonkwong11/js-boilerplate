55e3a05f72992ff448df1931684e240dbcb03105
Jason <jasonkwong11@gmail.com>
2017-04-25 17:34:19 -0400

    hooked up redux using immutable and fetch

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,7 +1,19 @@
 // @flow
 
 import React from 'react'
+import HelloButton from './container/hello-button'
+import HelloAsyncButton from './container/hello-async-button'
+import Message from './container/message'
+import MessageAsync from './container/message-async'
+import { APP_NAME } from '../shared/config'
 
-const App = () => <div><h1>Good Morning Jason!!</h1><h2>Hello from React</h2></div>
+const App = () =>
+  <div>
+    <h1>{APP_NAME}</h1>
+    <Message />
+    <HelloButton />
+    <MessageAsync />
+    <HelloAsyncButton />
+  </div>
 
 export default App
