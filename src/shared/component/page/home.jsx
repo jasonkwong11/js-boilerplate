fdbd6c7f6afbfbb7d28f4b47947e3f47f670ffb2
Jason <jasonkwong11@gmail.com>
2017-04-26 17:44:08 -0400

    add react helmet to title and meta tags

--- src/shared/component/page/home.jsx
+++ src/shared/component/page/home.jsx
@@ -1,7 +1,18 @@
 // @flow
 
 import React from 'react'
+import Helmet from 'react-helmet'
 
-const HomePage = () => <p>Home</p>
+import { APP_NAME } from '../../config'
+
+const HomePage = () =>
+  <div>
+    <Helmet
+      meta={[
+        { name: 'description', content: 'Hello App is an app to say hello' },
+        { property: 'og:title', content: APP_NAME },
+      ]}
+    />
+  </div>
 
 export default HomePage
